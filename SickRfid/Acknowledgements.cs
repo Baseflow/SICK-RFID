9e71ab8cab05bb961c18d879a8329d45a291db7f
EjaYF <ernstjan@baseflow.com>
2024-05-28 11:12:24 +0200

    Made some fields public in order to use the mock in another project

--- SickRfid/Acknowledgements.cs
+++ SickRfid/Acknowledgements.cs
@@ -3,15 +3,15 @@ namespace SickRfid;
 /// <summary>
 ///     Contains the acknowledgements that can be received from the Sick RFID reader.
 /// </summary>
-internal static class Acknowledgements
+public static class Acknowledgements
 {
     /// <summary>
     ///     The acknowledgement that the Sick RFID reader sends when it starts scanning for RFID tags.
     /// </summary>
-    internal const string ACK_START = "\u0002sAN MIStartIn\u0003";
+    public const string ACK_START = "\u0002sAN MIStartIn\u0003";
     
     /// <summary>
     ///     The acknowledgement that the Sick RFID reader sends when it stops scanning for RFID tags.
     /// </summary>
-    internal const string ACK_STOP = "\u0002sAN MIStopIn\u0003";
+    public const string ACK_STOP = "\u0002sAN MIStopIn\u0003";
 }
